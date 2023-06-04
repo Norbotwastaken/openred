2a2cc767ea2bbcc8294a54d218c2ed20025d79ab
Norbert Antal <anno95@ymail.com>
2023-06-04 15:38:10 +0200

    Initial Commit

---
+++ openred/openredApp.swift
@@ -0,0 +1,17 @@
+//
+//  openredApp.swift
+//  openred
+//
+//  Created by Norbert Antal on 6/4/23.
+//
+
+import SwiftUI
+
+@main
+struct openredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
